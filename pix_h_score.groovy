e266edccdfd984a237bd480ab361988ddc19c492
Rolf Harkes <rolf@harkes.nu>
2022-12-14 15:56:34 +0100

    Update pix_h_score.groovy

--- pix_h_score.groovy
+++ pix_h_score.groovy
@@ -13,8 +13,7 @@ println(w*h)
 var raster = img.getRaster()
 float[] px = new float[raster.getNumBands()]
 threshold_heam=0.3
-threshold_dab1=0.2
-threshold_dab2=0.4
+threshold_dab=[0.1, 0.2, 0.3]
 def n_heam = 0.0
 def n_dab_h = 0.0
 def n_dab_m = 0.0
@@ -22,18 +21,15 @@ def n_dab_l = 0.0
 for (int y = 0; y < h; y++) {
     for (int x = 0; x < w; x++) {
         px = raster.getPixel(x, y, px)
-        if (!(Float.isNaN(px[0]))){
-            if (px[0] > threshold_heam){
-                n_heam += 1
-            }
-        }
         if (!(Float.isNaN(px[1]))) {
-            if (px[1] > threshold_dab2) {
+            if (px[1] > threshold_dab[2]) {
                 n_dab_h += 1
-            } else if (px[1] > threshold_dab1) {
+            } else if (px[1] > threshold_dab[1]) {
                 n_dab_m += 1
-            } else {
+            } else if (px[1] > threshold_dab[0]) {
                 n_dab_l += 1
+            } else if(px[0] > threshold_heam){
+                n_heam += 1
             }
         }
     }
